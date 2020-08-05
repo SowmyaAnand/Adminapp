77543a33e26e8bcbaeda1803d847cfe7b0e803f6
SowmyaAnand <sowmyaanand2509@gmail.com>
2020-08-05 20:32:58 +0530

    searchview

--- app/src/main/java/com/dailyestoreapp/adminapp/Fragment1.java
+++ app/src/main/java/com/dailyestoreapp/adminapp/Fragment1.java
@@ -6,25 +6,31 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 public class Fragment1  extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    ArrayList Images_offers = new ArrayList<>(Arrays.asList(R.drawable.ic_menu_camera,R.drawable.ic_menu_camera, R.drawable.ic_menu_camera, R.drawable.ic_menu_camera, R.drawable.ic_menu_camera));
 
+    ArrayList personNames_offers = new ArrayList<>(Arrays.asList("ITEM1", "ITEM2", "ITEM3", "ITEM4", "ITEM5", "ITEM6", "ITEM7"));
+    RecyclerView recyclerView_offers,itemlistingcategory_offers;
+    LinearLayoutManager linearLayoutManager_offers,linearLayoutManager2_offers;
+    Offers_ItemAdapter customAdapter_offers;
+    OffersListingsubcategoryadapter customadapter2_offers;
     public Fragment1() {
         // Required empty public constructor
     }
@@ -62,10 +68,25 @@ public class Fragment1  extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-       SearchView sr = (SearchView)rootView.findViewById(R.id.searchview);
-sr.onActionViewExpanded();
 
-        sr.setQueryHint("Type here to search");
+        itemlistingcategory_offers = (RecyclerView) rootView.findViewById(R.id.recyclerView_categories_offer);
+        // set a LinearLayoutManager with default vertical orientation
+        LinearLayoutManager linearLayoutManager2_offers = new LinearLayoutManager(rootView.getContext(),LinearLayoutManager.HORIZONTAL,false);
+        itemlistingcategory_offers.setLayoutManager(linearLayoutManager2_offers);
+        //  call the constructor of CustomAdapter to send the reference and data to Adapter
+        customadapter2_offers = new OffersListingsubcategoryadapter(rootView.getContext(), personNames_offers,Images_offers);
+        itemlistingcategory_offers.setAdapter(customadapter2_offers);
+        //second recyclerview
+        recyclerView_offers = (RecyclerView) rootView.findViewById(R.id.itemrecycler_offers);
+        // set a LinearLayoutManager with default vertical orientation
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(rootView.getContext());
+        recyclerView_offers.setLayoutManager(linearLayoutManager);
+        //  call the constructor of CustomAdapter to send the reference and data to Adapter
+
+        customAdapter_offers = new Offers_ItemAdapter(rootView.getContext(), personNames_offers);
+        recyclerView_offers.setAdapter(customAdapter_offers);
+        // GridView gridview = (GridView) rootView.findViewById(R.id.gridview);
+        // gridview.setAdapter(new ImageAdapter(rootView.getContext()));
         return rootView;
     }
 }
---
+++ app/src/main/java/com/dailyestoreapp/adminapp/Offers_ItemAdapter.java
@@ -0,0 +1,117 @@
+package com.dailyestoreapp.adminapp;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Locale;
+public class Offers_ItemAdapter extends RecyclerView.Adapter<Offers_ItemAdapter.MyViewHolder> {
+    ArrayList<String> personNames = new ArrayList<String>();
+    Context context;
+    ArrayList<String> lts=new ArrayList<String>();
+    ArrayList personNames_offers = new ArrayList<>(Arrays.asList("ITEM1", "ITEM2", "ITEM3", "ITEM4", "ITEM5", "ITEM6", "ITEM7"));
+    int quantity=1;
+    public Offers_ItemAdapter(Context context, ArrayList personNames) {
+        this.context = context;
+        this.personNames = personNames;
+        this.lts.addAll(personNames);
+
+    }
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // infalte the item Layout
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.offer_items, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        MyViewHolder vh = new MyViewHolder(v); // pass the view to View Holder
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
+
+        // set the data in items
+        String name = (String) personNames.get(position);
+        holder.name.setText(name);
+        holder.addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.addbtn.setVisibility(View.GONE);
+            }
+        });
+        holder.addition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String q = String.valueOf(holder.quantityy.getText());
+                quantity= Integer.parseInt(q);
+                quantity=quantity+1;
+                String stringquantity = String.valueOf(quantity);
+                holder.quantityy.setText(stringquantity);
+            }
+        });
+        holder.substraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String q2 = String.valueOf(holder.quantityy.getText());
+                quantity= Integer.parseInt(q2);
+                if(quantity>1)
+                {
+                    quantity=quantity-1;
+                }
+                String stringquantity2 = String.valueOf(quantity);
+                holder.quantityy.setText(stringquantity2);
+            }
+        });
+        // implement setOnClickListener event on item view.
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return personNames.size();
+    }
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name,quantityy;// init the item view's
+        Button addition,substraction,addbtn;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.Title);
+            addition=(Button) itemView.findViewById(R.id.plus);
+            substraction = (Button) itemView.findViewById(R.id.minus);
+            quantityy = (TextView) itemView.findViewById(R.id.quantity);
+            addbtn = (Button) itemView.findViewById(R.id.add);
+            // get the reference of item view's
+
+        }
+    }
+    public void filter(String charText) {
+        Log.e("texting if","persons="+charText);
+        charText = charText.toLowerCase(Locale.getDefault());
+        Log.e("texting if2","persons="+charText);
+        personNames.clear();
+        Iterator itr=personNames.iterator();
+        if (charText.length() == 0) {
+            Log.e("texting if3","persons="+charText);
+            personNames.addAll(lts);
+        } else {
+            for(int i =0;i<lts.size();i++) {
+                Log.e("texting else","persons="+lts.get(i));
+                String s = (String) lts.get(i);
+                if (s.toLowerCase(Locale.getDefault()).contains(charText)) {
+                    personNames.add(s);
+                }
+            }
+        }
+        Log.e("text","persons="+personNames);
+        notifyDataSetChanged();
+    }
+}
